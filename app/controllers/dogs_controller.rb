d6e1f1d88679e4eb92af20df118595dc10af4253
Julie Kovalaske <juliekovalaske@gmail.com>
2019-10-25 15:02:52 +0000

    simple form

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,22 @@
 class DogsController < ApplicationController
 
   def index
+    @dogs = Dog.all
+  end
+
+  def new
+    @dogs = Dog.new
+  end
+
+  def create
+    Dog.create(dog_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age, :funfact)
   end
 
 end
